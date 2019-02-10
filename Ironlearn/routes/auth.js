86f172482c3592b5381accb0bbdeb2d7849c5e96
Amelia Lim <limchornguan418@gmail.com>
2019-02-10 13:21:08 +0100

    delete unnecessary images, fix logo

--- Ironlearn/routes/auth.js
+++ Ironlearn/routes/auth.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const passport = require('passport');
-const router = express.Router();
 const User = require("../models/User");
-
-// Bcrypt to encrypt passwords
 const bcrypt = require("bcryptjs");
+
+const router = express.Router();
 const bcryptSalt = 10;
 
 
