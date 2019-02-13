9f562df1ce4be9566e8c70fbfaf89d99afd71b58
Ruby Camille <Ruby@Rubys-MacBook-Air.local>
2019-02-13 22:13:23 +0100

    minor updates on profile

--- routes/profile.js
+++ routes/profile.js
@@ -10,30 +10,49 @@ const Like = require('../models/Like')
  ************************************/
 // GET '/profile'
 // ==> view profile, show 3 latest posts and likes
+// router.get('/', checkConnected, (req, res, next) => {
+//   Tutorial.find({ _creator: req.user._id }).sort({ "created_at": -1 }).limit(3)
+//     .then(post => {
+//       res.render('profile/index', { post, user: req.user })
+//     })
+//     .catch(err => {
+//       console.log("opps, something went wrong");
+//       next(err);
+//     });
+// })
+// /************************************
+//  * PROFILE PAGE Shows 3 likes
+//  ************************************/
+// router.get('/', checkConnected, (req, res, next) => {
+//   Like.find({ _user: req.user._id }).sort({ "created_at": -1 }).limit(3)
+//     .populate("_tutorial")
+//     .then(likes => {
+//       console.log(likes)
+//       res.render('profile/index', { likes })
+//     })
+// })
+
+
 router.get('/', checkConnected, (req, res, next) => {
-  Tutorial.find({ _creator: req.user._id }).sort({ "created_at": -1 }).limit(3)
-    .then(post => {
-      res.render('profile/index', { post, user: req.user })
-    })
-    .catch(err => {
-      console.log("opps, something went wrong");
-      next(err);
-    });
-})
-/************************************
- * PROFILE PAGE Shows 3 likes
- ************************************/
-router.get('/', checkConnected, (req, res, next) => {
-  Like.find({ _user: req.user._id }).sort({ "created_at": -1 }).limit(3)
+  Promise.all([Tutorial.find({ _creator: req.user._id }).sort({ "created_at": -1 }).limit(3), Like.find({ _user: req.user._id }).sort({ "created_at": -1 }).limit(3)
     .populate("_tutorial")
-    .then(likes => {
-      console.log(likes)
-      res.render('profile/index', { likes })
+  ])
+    .then(([posts, likes]) => {
+      console.log(posts);
+      console.log(likes);
+      res.render('profile/index', {
+        user: req.user,
+        likes,
+        posts
+
+      })
     })
 })
 
+
+
 /************************************
- * See all Tutorial (protected)
+ * See all Posts (protected)
  ************************************/
 // GET '/profile/allposts'
 router.get('/allposts', checkConnected, (req, res, next) => {
@@ -43,7 +62,7 @@ router.get('/allposts', checkConnected, (req, res, next) => {
         res.render('profile/allposts', { post, user: req.user })
       } else {
 
-      res.render('profile/allposts', { post, user: req.user })
+        res.render('profile/allposts', { post, user: req.user })
       }
     })
     .catch(err => {
